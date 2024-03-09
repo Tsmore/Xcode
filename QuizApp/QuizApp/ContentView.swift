e694ecfa5dc1d48f1e571ac73a0ba3e9eac4420f
Tsmore <143774064+Tsmore@users.noreply.github.com>
2024-03-09 21:02:00 +0900

    整理＋新アプリ

---
+++ QuizApp/QuizApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuizApp
+//
+//  Created by Tomonori Sampei on 2024/03/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuizApp/QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by Tomonori Sampei on 2024/03/09.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
