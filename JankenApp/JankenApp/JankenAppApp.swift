c9d2d8a5f73c6e810a7b3d26bc942e99df10f38d
Tsmore <143774064+Tsmore@users.noreply.github.com>
2024-03-10 21:51:50 +0900

    じゃんけんアプリ

---
+++ JankenApp/JankenApp/JankenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JankenAppApp.swift
+//  JankenApp
+//
+//  Created by Tomonori Sampei on 2024/03/10.
+//
+
+import SwiftUI
+
+@main
+struct JankenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
