b9ca8aa8714f3e41602a6735d29aa178ef5a8b78
Tsmore <143774064+Tsmore@users.noreply.github.com>
2024-03-11 21:07:44 +0900

    音楽アプリ(楽器)

---
+++ MusicApp/MusicApp/MusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppApp.swift
+//  MusicApp
+//
+//  Created by Tomonori Sampei on 2024/03/11.
+//
+
+import SwiftUI
+
+@main
+struct MusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
