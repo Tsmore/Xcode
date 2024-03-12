b5d58c0f36452a0cd6e6aec96829ecdb498ff9cd
Tsmore <143774064+Tsmore@users.noreply.github.com>
2024-03-12 22:58:16 +0900

    マップに検索機能

--- MapApp/MapApp/ContentView.swift
+++ MapApp/MapApp/ContentView.swift
@@ -8,9 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    //入力中の文字列を保持する状態変数
+    @State var inputText: String = ""
+    //検索キーワードを保持する状態変数、初期値は"東京駅"
+    @State var displaySearchKey: String = "東京駅"
     var body: some View {
         VStack {
-            MapView(searchKey: "羽田空港")
+            //テキストフィールド（文字入力）
+            TextField("キーワード", text: $inputText, prompt: Text("キーワードを入力してください"))
+                //入力が完了した時
+                .onSubmit {
+                    //入力が完了したから検索キーワードに設定する
+                    displaySearchKey = inputText
+                }
+                .padding()
+            MapView(searchKey: displaySearchKey)
         }
     }
 }
