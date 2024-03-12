e150b2af9dfdec7c6387baa3eb337fe9770b4f8a
Tsmore <143774064+Tsmore@users.noreply.github.com>
2024-03-12 21:43:19 +0900

    マップと検索

---
+++ MapApp/MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Tomonori Sampei on 2024/03/12.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
