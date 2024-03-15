21c8c4f17ed11b080a1829829293deb4fdf97443
Tsmore <143774064+Tsmore@users.noreply.github.com>
2024-03-15 23:13:30 +0900

    タイマーアプリ

---
+++ Timer/Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Tomonori Sampei on 2024/03/15.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
