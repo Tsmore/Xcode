1195b3f9f2776b34da056a9e5e75b8655f747d7d
Tsmore <143774064+Tsmore@users.noreply.github.com>
2024-03-15 23:51:10 +0900

    ボタンを追加

--- Timer/Timer/ContentView.swift
+++ Timer/Timer/ContentView.swift
@@ -9,13 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ZStack {
+                Image(.backgroundTimer)
+                    .resizable()
+                    .ignoresSafeArea()
+                    .scaledToFill()
+                VStack(spacing: 30.0) {
+                    Text("残り10秒")
+                        .font(.largeTitle)
+                    HStack {
+                        Button {
+                            
+                        } label: {
+                            Text("スタート")
+                                //文字サイズを指定
+                                .font(.title)
+                                //文字色を白に指定
+                                .foregroundStyle(Color.white)
+                                //幅と高さを140に指定
+                                .frame(width: 140, height: 140)
+                                //背景を設定
+                                .background(Color.start)
+                                //円形切り抜く
+                                .clipShape(Circle())
+                        }
+                        Button {
+                            
+                        } label: {
+                            Text("ストップ")
+                                .font(.title)
+                                .foregroundStyle(Color.white)
+                                .frame(width: 140, height: 140)
+                                .background(Color.stop)
+                                .clipShape(Circle())
+                        }
+                    }
+                }
+            }
+            //ナビゲーションにボタンを追加
+            .toolbar {
+                //ナビゲーションバーの右にボタンを追加
+                ToolbarItem(placement: .topBarTrailing) {
+                    //ナビゲーション遷移
+                    NavigationLink {
+                        SettingView()
+                    } label: {
+                        Text("秒数設定")
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
---
+++ Timer/Timer/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  Timer
+//
+//  Created by Tomonori Sampei on 2024/03/15.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
