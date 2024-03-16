6a784c6a1c494ae09b5abc76400d4afa861e6d67
Tsmore <143774064+Tsmore@users.noreply.github.com>
2024-03-17 02:33:50 +0900

    カメラアプリ

---
+++ Camera/Camera/CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraApp.swift
+//  Camera
+//
+//  Created by Tomonori Sampei on 2024/03/17.
+//
+
+import SwiftUI
+
+@main
+struct CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
