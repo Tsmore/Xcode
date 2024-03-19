ab2d6e4489901ec63823936940a03d1324d321d9
Tsmore <143774064+Tsmore@users.noreply.github.com>
2024-03-19 23:19:22 +0900

    シェアできるように

--- Camera/Camera/ContentView.swift
+++ Camera/Camera/ContentView.swift
@@ -61,6 +61,26 @@ struct ContentView: View {
                 // UIImagePickerController（写真撮影）を表示
                 ImagePickerView(isShowSheet: $isShowSheet, captureImage: $captureImage)
             }
+            // captureImageをアンラップする
+            if let captureImage {
+                // captureImageから共有する画像を生成する
+                let shareImage = Image(uiImage: captureImage)
+                // 共有シート
+                ShareLink(item: shareImage, subject: nil, message: nil, preview: SharePreview("Photo", image: shareImage)) {
+                    // テキスト表示
+                    Text("SNSに投稿する")
+                        // 横幅いっぱい
+                        .frame(maxWidth: .infinity)
+                        // 高さ50ポイント指定
+                        .frame(height: 50)
+                        // 背景を青色に指定
+                        .background(Color.blue)
+                        // 文字色を白色に指定
+                        .foregroundStyle(Color.blue)
+                        // 上下左右に余白を追加
+                        .padding()
+                }
+            }
         }
     }
 }
