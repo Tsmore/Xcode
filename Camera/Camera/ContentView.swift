c7d1c675824561809f5a66459b65de03e1d6fa22
Tsmore <143774064+Tsmore@users.noreply.github.com>
2024-03-21 21:55:50 +0900

    フォトライブラリーから選択できる機能

--- Camera/Camera/ContentView.swift
+++ Camera/Camera/ContentView.swift
@@ -6,12 +6,15 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
     // 撮影した写真を保持する状態変数
     @State var captureImage: UIImage? = nil
     // 撮影画面(sheet)の開閉状態を管理
     @State var isShowSheet = false
+    
+    @State var photoPickerSelectedImage: PhotosPickerItem? = nil
     var body: some View {
         VStack {
             // スペース追加
@@ -61,6 +64,40 @@ struct ContentView: View {
                 // UIImagePickerController（写真撮影）を表示
                 ImagePickerView(isShowSheet: $isShowSheet, captureImage: $captureImage)
             }
+            // フォトライブラリーから選択する
+            PhotosPicker(selection: $photoPickerSelectedImage, matching: .images, preferredItemEncoding: .automatic, photoLibrary: .shared()) {
+                // テキスト表示
+                Text("フォトライブラリーから選択する")
+                // 横幅いっぱい
+                    .frame(maxWidth: .infinity)
+                // 高さ50ポイント指定
+                    .frame(height: 50)
+                // 背景を青色に指定
+                    .background(Color.blue)
+                // 文字色を白色に指定
+                    .foregroundStyle(Color.white)
+                // 上下左右に余白を追加
+                    .padding()
+            }
+            // 選択した写真情報をもとに写真を取り出す
+            .onChange(of: photoPickerSelectedImage, initial: true, { oldValue, newValue in
+                // 選択した写真があるとき
+                if let newValue {
+                    // Data型で写真を取り出す
+                    newValue.loadTransferable(type: Data.self) { result in
+                        switch result {
+                        case .success(let data):
+                            // 写真があるとき
+                            if let data {
+                                // 写真をcaptureImageに保存
+                                captureImage = UIImage(data: data)
+                            }
+                        case .failure:
+                            return
+                        }
+                    }
+                }
+            })
             // captureImageをアンラップする
             if let captureImage {
                 // captureImageから共有する画像を生成する
