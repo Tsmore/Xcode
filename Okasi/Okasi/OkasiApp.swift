a69789bee0693e9b5dc8d29df8688d5ec8051d68
Tsmore <143774064+Tsmore@users.noreply.github.com>
2024-03-25 21:57:58 +0900

    お菓子検索

---
+++ Okasi/Okasi/OkasiApp.swift
@@ -0,0 +1,17 @@
+//
+//  OkasiApp.swift
+//  Okasi
+//
+//  Created by Tomonori Sampei on 2024/03/25.
+//
+
+import SwiftUI
+
+@main
+struct OkasiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
